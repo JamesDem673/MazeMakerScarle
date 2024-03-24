5d1b0c4da1efc238feee496981bf9196d8e26c99
JamesDem673 <james2.dempsey@live.uwe.ac.uk>
2024-03-24 14:11:00 +0000

    Added Files

---
+++ Source/MazeMakerForScarle/ReadToFile.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MAZEMAKERFORSCARLE_API ReadToFile
+{
+public:
+	ReadToFile();
+	~ReadToFile();
+};
